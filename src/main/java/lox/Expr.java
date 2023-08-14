d63525da0144bc6c14fcad1e93da6c30e596ad73
Guilherme Pereira <guilhermepereira2917@gmail.com>
2023-08-14 00:35:20 -0300

    :sparkles: added tool GenerateAst to automatically generate Expr class code and added Expr class

---
+++ src/main/java/lox/Expr.java
@@ -0,0 +1,70 @@
+package main.java.lox;
+
+public abstract class Expr {
+    public interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    public static class Binary extends Expr {
+        public final Expr left;
+        public final Token operator;
+        public final Expr right;
+
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+    }
+
+    public static class Grouping extends Expr {
+        public final Expr expression;
+
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+    }
+
+    public static class Literal extends Expr {
+        public final Object value;
+
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+    }
+
+    public static class Unary extends Expr {
+        public final Token operator;
+        public final Expr right;
+
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+    }
+
+    public abstract <R> R accept(Visitor<R> visitor);
+}
---
+++ src/main/java/tool/GenerateAst.java
@@ -0,0 +1,89 @@
+package main.java.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+
+        String outputDirectory = args[0];
+
+        defineAst(outputDirectory, "Expr", Arrays.asList(
+                        "Binary   : Expr left, Token operator, Expr right",
+                        "Grouping : Expr expression",
+                        "Literal  : Object value",
+                        "Unary    : Token operator, Expr right"
+                ));
+    }
+
+    private static void defineAst(String outputDirectory, String baseName, List<String> types) throws IOException {
+        String path = String.format("%s/%s.java", outputDirectory, baseName);
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package main.java.lox;");
+        writer.println();
+        writer.println("public abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("    public abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.println("}");
+
+        writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    public interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+        writer.println("");
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    public static class " + className + " extends " + baseName + " {");
+
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            writer.println("        public final " + field + ";");
+        }
+
+        writer.println();
+        writer.println("        " + className + "(" + fieldList + ") {");
+
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        writer.println("        }");
+
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        public <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        writer.println("    }");
+        writer.println();
+    }
+}
